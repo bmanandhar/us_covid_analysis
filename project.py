bed69d82483f6563d8b6b94b112720bccd2dce7e
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2020-09-25 11:39:15 -0700

    state wise cases, country total

--- project.py
+++ project.py
@@ -12,11 +12,32 @@ import matplotlib.pyplot as plt
 #date,county,state,fips,cases,deaths
 sum = 0
 x = input("Enter county name: ")
-with open("/Users/bijayamanandhar/Desktop/us_covid_analysis/us_counties_covid19_daily.csv","r") as data:
+
+with open("us_counties_covid19_daily.csv","r") as data:
+# with open("/Users/bijayamanandhar/Desktop/us_covid_analysis/us_counties_covid19_daily.csv","r") as data:
     csv_reader = csv.reader(data)
     next(csv_reader)
     for line in csv_reader:
         if x == line[1]:
             sum += int(line[4])
     print(x,"Cases: ", sum)
+
+z = input("Do you want state wise covid19 cases (y/n)? ")
+if z == 'y':
+    state_wise_cases = dict()
+    country_total = 0
+    with open("us_counties_covid19_daily.csv","r") as data:
+        csv_reader = csv.reader(data)
+        next(csv_reader)
+        for line in csv_reader:
+            country_total += int(line[4])
+            if line[2] in state_wise_cases:
+                state_wise_cases[line[2]] += int(line[4])
+            else:
+                state_wise_cases[line[2]] = int(line[4])
+    print("State wise total: {}\n=*=*=*=*=*\nCountry total: {}".format(state_wise_cases, country_total))
+else: print("End of process, thank you!Alameda
+")
+
+
 
